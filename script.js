6577723d86f79cc84b267b162ef6d0a5374dd888
opal2718 <littlesihoo@gmail.com>
2024-03-14 23:09:28 +0900

    1.1.20

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function changeHTML(){
     }
     else if (mode == "Quiz_Korean") {
       totalHTML += ((i+1)+". "+split_blanks_1[i] + '<input autocomplete="off" onkeyup="check(' + i + ')" type="text" id="blank' + i + '">' + split_blanks_2[i] + "<br>");
-      totalHTML += (sentences_kor[i] + "<br><br><br>");
+      totalHTML += ("&nbsp;&nbsp;"+sentences_kor[i] + "<br><br><br>");
     }
     else if (mode == "Quiz") {
       totalHTML += ((i+1)+". "+split_blanks_1[i] + '<input autocomplete="off" onkeyup="check(' + i + ')" type="text" id="blank' + i + '">' + split_blanks_2[i] + "<br><br><br>");
