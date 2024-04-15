8f583a960b6a8cabcc53f24cef1bd3de51db030e
opal2718 <littlesihoo@gmail.com>
2024-04-15 11:50:57 +0900

    1.8.2

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function changeHTML(){
     }
     else if (mode == "Quiz_Korean_First") {
       totalHTML += ((i+1)+". "+split_blanks_1[i] + '<input value="'+answers[i][0]+'" autocomplete="off" onkeyup="check(' + i + ')" type="text" id="blank' + i + '">' + split_blanks_2[i] + "<br><br><br>");
+      totalHTML += ("&nbsp;&nbsp;"+sentences_kor[i] + "<br><br><br>");
     }
   }
   return totalHTML;
