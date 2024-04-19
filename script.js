db6459734ff1a40eed8767215584aaad85cf5d68
opal2718 <littlesihoo@gmail.com>
2024-04-19 13:48:25 +0900

    1.13.2

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ function check_reversed(n) {
       var tttt = 0;
       var related = [];
       for(tttt = 0; tttt < words.length; tttt++){
-        if(words[tttt].toLowerCase().contains(answerV.toLowerCase().trim())) related.push(words[tttt])
+        if((words[tttt].toLowerCase()).contains((answerV.toLowerCase()).trim())) related.push(words[tttt])
       }
       var ttttt = 0;
       var optionsArr = "";
