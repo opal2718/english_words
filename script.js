6e9e56ed20c07d9ca88fd6c95bb49449e4b7e379
opal2718 <littlesihoo@gmail.com>
2024-04-30 21:42:11 +0900

    1.14.3

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function check_reversed(n) {
     }
   }
 }
-
+/*
 var date = new Date();
 var year=date.getFullYear(), month=date.getMonth()+1, day=date.getDate(), hour=date.getHours(), minute=date.getMinutes();
 var dyear = 2024, dmonth=4, dday=22;
@@ -157,7 +157,7 @@ var monthLength=[0,31,29,31,30,31,30,31,31,30,31,30,31];
 var dayLeft = dday-day;
 for(var leftDI=month; leftDI<dmonth; leftDI++) dayLeft+=monthLength[leftDI];
 if(dayLeft >= 0) $("#dday").html("D-"+(dayLeft));
-else $("#dday").html("D+"+(dayLeft));
+else $("#dday").html("D+"+(-dayLeft));*/
 
 $("#answer").change(function(){
   if($("#answer").is(":checked")){
